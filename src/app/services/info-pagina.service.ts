e88cb9c4ddc99ee19b63ddbfd1e88232b7ea41f4
Nelson <n00gomez00@gmail.com>
2019-06-23 22:53:44 -0600

    se agrego firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,13 +8,27 @@ import { infoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: infoPagina={};
   cargada = false;
+  equipo: any[]= [];
   constructor(private http: HttpClient) {
     console.log('Servicio pagina cargado');
-
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+   }
+   private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: infoPagina)=>{
       this.cargada = true;
       this.info = resp;
+     
+    });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-bd56e.firebaseio.com/equipo.json')
+    .subscribe((resp: any[])=>{
+      this.equipo = resp;
+      
      console.log(resp);
     });
    }
