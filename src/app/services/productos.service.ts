c6b4363db040fee0853c622d0a9c794575364cb3
Nelson <n00gomez00@gmail.com>
2019-06-24 16:28:30 -0600

    search

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,22 +9,53 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 cargando = true;
 productos: Producto[] = [];
+productoFiltrado: Producto[]=[];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
    }
 
    private cargarProductos(){
-     this.http.get('https://angular-html-bd56e.firebaseio.com/productos_idx.json')
-     .subscribe((resp: Producto[])=>{
-       console.log(resp);
-       this.productos= resp;
-       this.cargando = false;
 
-      //  setTimeout(()=>{
-         this.cargando = false;
-      //  }, 2000);
-     });
-    }
+    return new Promise((resolve, reject)=>{
+      this.http.get('https://angular-html-bd56e.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[])=>{
+        this.productos= resp;
    
+          this.cargando = false;
+          resolve();
+       
+      });
+      
+    });
+
+
+    }
+   getProducto(id: string){
+    return this.http.get(`https://angular-html-bd56e.firebaseio.com/productos/${id}.json`);
+    
+   }
+   buscarProducto(termino: string){
+     if(this.productos.length=== 0){
+      this.cargarProductos().then(()=>{
+
+        this.filtrarProductos(termino);
+      });
+     }else{
+      this.filtrarProductos(termino);
+     }
+
+   }
+   private filtrarProductos(termino: string){
+
+this.productoFiltrado= [];
+termino = termino.toLocaleLowerCase();
+
+this.productos.forEach(prod=>{
+  const tituloLower = prod.titulo.toLocaleLowerCase();
+if(prod.categoria.indexOf(termino)>=0 || tituloLower.indexOf(termino)>=0){
+  this.productoFiltrado.push(prod);
+}
+});
+   }
 }
