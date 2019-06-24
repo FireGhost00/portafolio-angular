2357f859b2e7520ba34ade32e005fdbb99858487
Nelson <n00gomez00@gmail.com>
2019-06-23 21:25:27 -0600

    se agrego animacion

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,16 @@
+
+
+
+
+
+export interface infoPagina{
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
